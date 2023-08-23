6dccfe433e742172f167f592e5769230aefa3c30
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-08-23 16:11:05 +0200

    Update CarRepositoryTest.java

--- src/test/java/dat3/car/repository/CarRepositoryTest.java
+++ src/test/java/dat3/car/repository/CarRepositoryTest.java
@@ -1,7 +1,9 @@
 package dat3.car.repository;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +11,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class CarRepositoryTest {
 
+    @Autowired
+    CarRepository carRepository;
+
+    @BeforeAll
+
+
     @Test
     public void findByIdTest(){
+        carRepository.findById(1);
+    }
 
-    }
-    @AfterEach
-    void tearDown() {
-    }
 }
